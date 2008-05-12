a9b39f48c561b9c6971fd0bfe1d89c52e36689a3
Kyle Neath <kneath@gmail.com>
2008-05-12 00:20:24 -0700

    Fix bar graph to list in right order; fit bar graph inside wrapper

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -13,6 +13,7 @@ module Merb
         query = "http://chart.apis.google.com/chart?cht=bvs&chs=#{options[:size]}&chco=#{options[:color]}"
         query << "&chd=t:" + collection.collect{|d| d.send(y_method) }.join(",")
         query << "&chds=0,#{max}"
+        query << "&chbh=14"
         query << "&chxt=x,y"
         query << "&chxl=0:|#{labels.join('|')}|1:|0|#{number_with_delimiter max/4}|#{number_with_delimiter max*2/4}|#{number_with_delimiter max*3/4}|#{number_with_delimiter max}"
         query << "&chg=" + [5000, 25, 1, 3].join(',')
