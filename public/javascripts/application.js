d007a33ede1ed45875a79925f077cd99f5d2652e
Kyle Neath <kneath@gmail.com>
2008-06-06 01:12:00 -0700

    Bring back the site selector

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,5 @@
 
 var rules = {
-  '#site_selector': function(element){
-    $(element).addEvent('change', function(){
-      if (this.value == null || this.value == "") return;
-      document.location = "/campaigns/show/?site_id=" + this.value;
-    });
-  },
   
   'input.datepicker': function(element){
     element = $(element);
